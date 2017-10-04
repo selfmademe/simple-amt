9ad58315bf991dd46ebdbabdf389968b2092ceec
Daniel Hauagge <daniel@selfmade.co>
2017-10-04 14:33:38 -0400

    x

--- show_hit_progress.py
+++ show_hit_progress.py
@@ -1,5 +1,6 @@
 import argparse
 from collections import Counter
+from progressbar import ProgressBar
 
 import simpleamt
 
@@ -17,12 +18,16 @@ if __name__ == '__main__':
     hit_ids = [line.strip() for line in f]
 
   counter = Counter()
-  for idx, hit_id in enumerate(hit_ids):
-    print 'Checking HIT %3d of %d' % (idx + 1, len(hit_ids))
+
+  pbar = ProgressBar(max_value=len(hit_ids))
+  missing_hits = []
+  for idx, hit_id in pbar(enumerate(hit_ids)):
+    # print 'Checking HIT %3d of %d' % (idx + 1, len(hit_ids))
     try:
       hit = mtc.get_hit(hit_id)[0]
     except:
-      print 'Can\'t find hit id: %d' % (hit_id)
+      missing_hits.append(hit_id)
+      # print 'Can\'t find hit id: %d' % (hit_id)
       continue
     total = int(hit.MaxAssignments)
     completed = 0
@@ -31,6 +36,10 @@ if __name__ == '__main__':
       if s == 'Submitted' or s == 'Approved' or s == 'Rejected':
         completed += 1
     counter.update([(completed, total)])
+
+  if len(missing_hits) > 0:
+    print "Could not find %d hits. Here's a sample of them:"
+    print ' '.join(missing_hits[:10])
 
   for (completed, total), count in counter.most_common():
     print '%d / %d: %d' % (completed, total, count)
